1aedc41f7e98f23fed44e7b5f610767e55769463
brofar <behfar@otafest.com>
2021-06-15 12:28:14 -0600

    Remove excess resource

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,14 +59,5 @@ namespace Controls_Randomizer.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
-        /// </summary>
-        internal static System.IO.UnmanagedMemoryStream wut {
-            get {
-                return ResourceManager.GetStream("wut", resourceCulture);
-            }
-        }
     }
 }
